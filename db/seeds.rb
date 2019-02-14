013e848596e4ea27fb5dd47307a15f8b345a15f3
Coralie Delpha <coralie.delpha@gmail.com>
2019-02-14 12:43:07 +0100

    Create seed by parsing the JSON list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+cocktail_ingredients = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredients = JSON.parse(cocktail_ingredients)
+
+ingredients.each do |ingredient|
+  ingredient[1].each do |element|
+    Ingredient.create(name: element['strIgrendient1'])
+  end
+end
